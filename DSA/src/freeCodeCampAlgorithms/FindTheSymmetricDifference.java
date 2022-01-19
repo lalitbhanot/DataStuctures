e820fd2d00744bb64ddd228e53eb6d847bbf7d7f
LalitBhanot <lalitthebest.gmail.com>
2022-01-19 11:04:30 +0530

    Inital Commit

--- DSA/src/freeCodeCampAlgorithms/FindTheSymmetricDifference.java
+++ DSA/src/freeCodeCampAlgorithms/FindTheSymmetricDifference.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 /* Problem:Find the Symmetric Difference
-The mathematical term symmetric difference (△ or ⊕) of two sets is the set of elements 
+The mathematical term symmetric difference (△ or ⊕)  of two sets is the set of elements 
 which are in either of the two sets but not in both. 
 
 For example, for sets A = {1, 2, 3} and B = {2, 3, 4}, A △ B = {1, 4}.
